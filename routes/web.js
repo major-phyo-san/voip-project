bbd2f2e75f95ea851a138c513c5053a2fa2a31bc
major-phyo-san <phyojupiter9@gmail.com>
2021-01-17 20:07:55 +0630

    admin panel routes and user crud routes added

--- routes/web.js
+++ routes/web.js
@@ -8,6 +8,7 @@ var LoginController = require('../app/http/controllers/auth/LoginController');
 var RegisterController = require('../app/http/controllers/auth/RegisterController');
 
 var AdminScreenController = require('../app/http/controllers/AdminScreenController');
+var UserCRUDController = require('../app/http/controllers/UserCRUDController');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -18,8 +19,15 @@ router.get('/', function(req, res) {
 router.get('/protected', authValidator.validateWebUser, function(req, res){
     res.send('authenticated user');
 });
+
+/* admin panel */
 router.get('/admins/communication/calls', authValidator.validateWebUser, webUserGuard.adminsOnly, AdminScreenController.callScreen);
-router.get('/admins/management/users', AdminScreenController.userManagementScreen);
+router.get('/admins/management/users', authValidator.validateWebUser, webUserGuard.adminsOnly, AdminScreenController.userManagementScreen);
+
+/* user CRUD routes */
+router.post('/users', authValidator.validateWebUser, webUserGuard.adminsOnly, UserCRUDController.addNewUser);
+router.delete('/users/:userId', authValidator.validateWebUser, webUserGuard.adminsOnly, UserCRUDController.deleteUser);
+router.put('/users/:userId', authValidator.validateWebUser, webUserGuard.adminsOnly, UserCRUDController.updateUser);
 
 /* Web authentication routes */
 router.get('/login', LoginController.showLoginForm);
