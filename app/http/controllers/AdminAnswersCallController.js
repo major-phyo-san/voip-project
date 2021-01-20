f6b570c3b4ab5e5ffb8a324d481fe8fe692565af
Phyo Kyaw San <phyojupiter9@gmail.com>
2021-01-20 21:25:17 +0630

    remote key retrieval

--- app/http/controllers/AdminAnswersCallController.js
+++ app/http/controllers/AdminAnswersCallController.js
@@ -1,6 +1,7 @@
 var database = require('../../../config/database');
 var Event = require('events');
 var stringGenerator = require('../../../utilities/stringGenerators');
+var numberGenerator = require('../../../utilities/numberGenerators');
 
 var envs = require('../../../config/server-env');
 const {RtcTokenBuilder, RtcRole} = require('agora-access-token');
@@ -10,6 +11,8 @@ const channelName = stringGenerator.generateRandomString(10);
 const uId = 0;
 const role = RtcRole.SUBSCRIBER;
 const privilegeExpireTime = Math.floor(Date.now() / 1000) + 3600;
+
+const superagent = require('superagent');
 
 var optionalConnectionString = {
     authSource: 'admin',
@@ -29,20 +32,52 @@ var adminAnswerEvent = new Event();
 module.exports.answerCall = function(req, res){
     if(req.body.answer === 'accept'){
         // fire accept call event with channel credentials
-        const channelToken = RtcTokenBuilder.buildTokenWithUid(agoraAppId, agoraAppCertificate, channelName, uId, role, privilegeExpireTime);
-        const channelCredentials = {
-            'channel_name': channelName,
-            'channel_token': channelToken
-        };
-        envelope = {
-            "data" : {
-                "agora_id": agoraAppId,
-                "channel_name": channelCredentials.channel_name,
-                "channel_token": channelCredentials.channel_token
+        uid = numberGenerator.generateRandomInt(100000, 999999);
+        channel = stringGenerator.generateRandomString(10);
+        superagent.get('https://pks-test-token.herokuapp.com/access_token')
+        .query({'channel': channel, 'uid': uid})
+        .end(function(err, superres){
+            var channelCredentials = Array();
+            if(err){
+                channelCredentials = {
+                    "channel_name": "",
+                    "channel_token": "",
+                    "channel_uid": ""
+                };
             }
-        };
-        adminAnswerEvent.emit('answer', 'accept', channelCredentials);
-        res.send(envelope);
+            else{
+                channelCredentials = {
+                    "channel_name": channel,
+                    "channel_token": superres.body.token,
+                    "channel_uid": uid
+                };
+            }
+
+            envelope = {
+                "data": {
+                    "agora_id": agoraAppId,
+                    "channel_name": channelCredentials.channel,
+                    "channel_token": channelCredentials.channel_token,
+                    "channel_uid": channelCredentials.uid
+                }
+            };
+            adminAnswerEvent.emit('answer', 'accept', channelCredentials);
+            res.send(envelope);
+        });
+        // const channelToken = RtcTokenBuilder.buildTokenWithUid(agoraAppId, agoraAppCertificate, channelName, uId, role, privilegeExpireTime);
+        // const channelCredentials = {
+        //     'channel_name': channelName,
+        //     'channel_token': channelToken
+        // };
+        // envelope = {
+        //     "data" : {
+        //         "agora_id": agoraAppId,
+        //         "channel_name": channelCredentials.channel_name,
+        //         "channel_token": channelCredentials.channel_token
+        //     }
+        // };
+        // adminAnswerEvent.emit('answer', 'accept', channelCredentials);
+        // res.send(envelope);
     }
     if(req.body.answer === 'reject'){
         // fire reject call event with null channel
