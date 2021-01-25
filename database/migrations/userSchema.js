9cf09f3eb92abfcbf6c23c70f0db41aab6c8c212
Phyo Kyaw San <phyojupiter9@gmail.com>
2021-01-25 20:40:17 +0630

    user schema updated

--- database/migrations/userSchema.js
+++ database/migrations/userSchema.js
@@ -4,6 +4,8 @@ const Schema = mongoose.Schema;
 var userSchema = new Schema({
     name: {type: String, required: true},
     email: {type: String, required: true, unique: true},
+    phone: {type: String, required: true, unique: true},
+    verified: {type: Boolean, required: true},
     password: {type: String, required: true, minlength: 8},
     role: {type: String, required: true}
 });
